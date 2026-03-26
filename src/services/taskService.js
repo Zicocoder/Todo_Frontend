f10aeb983c4f2dfd956aca029be8252fbe73ae2e
Zicocoder <Azzoug.zackaria@gmail.com>
2026-03-26 17:03:02 +0100

    feat: add create

--- src/services/taskService.js
+++ src/services/taskService.js
@@ -1,29 +1,39 @@
-    //Manages todo items via different methods
-    import axios  from 'axios';
-    import { authService } from './authService';
+//Manages todo items via different methods
+import axios from 'axios';
+import {authService} from './authService';
 
 
-    const API_URL = 'http://localhost:9090/api'
-    //authorization header with the Bearer token
-    const authHeader = ()  => ({
-        Authorization: `Bearer ${authService.getToken()}`
-    })
+const API_URL = 'http://localhost:9090/api'
+//authorization header with the Bearer token
+const authHeader = () => ({
+    Authorization: `Bearer ${authService.getToken()}`
+})
 
-    export const taskService = {
-        getAll: async () => {
-            const response = await axios.get(`${API_URL}/todo`, {
-                headers: authHeader()
-            })
-            return response.data;
-        },
-        getById: async (id) => {
-            const response = await axios.get(`${API_URL}/todo/${id}`, {
-                headers: authHeader()
-            })
-            return response.data;
-        },
+export const taskService = {
+    getAll: async () => {
+        const response = await axios.get(`${API_URL}/todo`, {
+            headers: authHeader()
+        })
+        return response.data;
+    },
+    getById: async (id) => {
+        const response = await axios.get(`${API_URL}/todo/${id}`, {
+            headers: authHeader()
+        })
+        return response.data;
+    },
 
-    }
+    // Make a new Todo, with optional files
+    create: async (todoDto, files = []) => {
+        const formData = new FormData();
+        formData.append('todo', new Blob([JSON.stringify(todoDto)], {type: 'application/json'}));
+        files.forEach(file => formData.append('files', file));
+        const response = await axios.post(`${API_URL}/todo`, formData, {
+            headers: authHeader()
+        })
+        return response.data;
+    },
+}
 
 
 
